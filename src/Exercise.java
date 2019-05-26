2e6f6fff95fc96813ec6bebdff1cb532c9b37cf9
andixiong <xiongandi@gmail.com>
2019-05-26 15:31:02 -0700

    Sample Exercise for John Knox

---
+++ src/Exercise.java
@@ -0,0 +1,85 @@
+
+import java.util.ArrayList;
+
+public class Exercise {
+    public static void main(String args[]) {
+        test1();
+        test2();
+        test3();
+    }
+
+    /**
+     * Given an arraylist of integers, find the length of the longest sub-sequence
+     * such that it is non-descending.
+     * ie. If the list is [1,1], you should return 2
+     * ie. If the list is [1,2,3,4,5,4,3,2,1], you should return 5
+     * ie. If the list is [5,4,3,2,1], you should return 0
+     * ie. If the list is [1,2,3,2,1,4,5,9,4,3,2,1], you should return 4
+     * @return the length of the longest non-descending sub-sequence
+     */
+    public static int LongestNonDescendingString(ArrayList<Integer> list) {
+        int longest = 0;
+        int count, j;
+
+        for(int i = 0; i < list.size(); i++){
+            j = 0;
+            count = 0;
+            while( j < list.size() && list.get(j) < list.get(j + 1)) {
+                count++;
+                j++;
+            }
+            if(count > longest){
+                longest = count;
+            }
+        }
+
+        return longest;
+    }
+
+    public static void test1() {
+        ArrayList<Integer> nums = new ArrayList<>(){{
+            add(1);
+            add(2);
+            add(3);
+        }};
+
+        if(LongestNonDescendingString(nums) == 3)
+            System.out.println("Passed test 1");
+        else
+            System.out.println("Failed test 1");
+    }
+
+    public static void test2() {
+        ArrayList<Integer> nums = new ArrayList<>(){{
+            add(2);
+            add(2);
+            add(1);
+            add(1);
+            add(2);
+            add(9);
+        }};
+
+        if(LongestNonDescendingString(nums) == 3)
+            System.out.println("Passed test 2");
+        else
+            System.out.println("Failed test 2");
+    }
+
+    public static void test3() {
+        ArrayList<Integer> nums = new ArrayList<>(){{
+            add(9);
+            add(8);
+            add(7);
+            add(6);
+            add(5);
+            add(4);
+        }};
+
+        if(LongestNonDescendingString(nums) == 0)
+            System.out.println("Passed test 3");
+        else
+            System.out.println("Failed test 3");
+    }
+
+    // add more tests of your own choice
+}
